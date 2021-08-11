a8ce43db3fb32ab1b6936d1556ff70baa553d4c9
yejiale <2499226042@qq.com>
2021-08-11 20:13:35 +0800

    before_zarmUI

---
+++ src/router/index.js
@@ -0,0 +1,14 @@
+import Index from '../container/Index';
+import About from '../container/About';
+
+const routes = [
+	{
+		path: '/',
+		component: Index,
+	},
+	{
+		path: '/about',
+		component: About,
+	},
+];
+export default routes;
